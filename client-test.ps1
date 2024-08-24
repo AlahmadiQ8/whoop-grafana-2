a290f6d93549b11fdde7dd5dba62d9ada2a6c155
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2024-08-24 21:35:50 +0300

    init

---
+++ client-test.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest `
+  -Uri https://whoopextractdownloadlink.azurewebsites.net/api/whoopextractdownloadlink?code=8Z1RuTJWowB42UWtrZuRBIX7oRFWQBnRZtUr05QXK3hmAzFuq0KAjw%3D%3D `
+  -Method POST `
+  -Body 'sdfsdfsdfsdfsd'
