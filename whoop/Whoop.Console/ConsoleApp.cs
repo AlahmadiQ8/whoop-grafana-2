d744f3fd44ce699bb1faa2e13688f7af366aa954
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2024-08-31 14:05:38 +0300

    Added new console project

---
+++ whoop/Whoop.Console/ConsoleApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Whoop.Console;
+
+public class ConsoleApp(IConfiguration configuration, ILogger<ConsoleApp> logger)
+{
+    public void Run(string[] args)
+    {
+        logger.LogInformation("Starting application");
+        logger.LogDebug("Debugging application");
+        System.Console.WriteLine("Hello World!");
+        System.Console.WriteLine(configuration.GetValue<string>("MyConfig"));
+    }
+}
---
+++ whoop/Whoop.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Whoop.Console;
+
+var appBuilder = Host.CreateApplicationBuilder(args);
+appBuilder.Services.AddSingleton<ConsoleApp>();
+using var app = appBuilder.Build();
+
+var consoleApp = app.Services.GetRequiredService<ConsoleApp>();
+
+consoleApp.Run(args);
