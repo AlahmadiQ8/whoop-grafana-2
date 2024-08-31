fc763b7b542002f09ef72de8baa1c8526109be3e
Mohammad Asad Mohammad <mmohammadq8@outlook.com>
2024-08-31 20:10:51 +0300

    undo new file changes

---
+++ whoop/open-api-generator/generate.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/zsh
+
+# get the directory of the root project. If running script in nested directory, change the number of `..`
+ROOT_DIR=$(dirname $(dirname $(dirname $(realpath $0))))
+
+echo $ROOT_DIR
+
+ openapi-generator-cli generate \
+     -g csharp \
+     -i ./whoop-openapi.yaml \
+     -c ./config.json \
+     -o ./Testing \
+     --global-property supportingFiles=false,apiTests=false,modelTests=false
--- whoop/scratch.md
+++ whoop/scratch.md
@@ -6,6 +6,14 @@
 openapi-generator-cli config-help -g csharp
 
 openapi-generator-cli generate -g csharp -i ./whoop-openapi.yaml -c ./config.json -o ./Testing --global-property supportingFiles=false,apiTests=false,modelTests=false --dry-run 
+```
 
+```
+Whoop.Sdk.sln
+appveyor.yml
+api/openapi.yaml
+.gitignore
+git_push.sh
+README.md
 ```
 
